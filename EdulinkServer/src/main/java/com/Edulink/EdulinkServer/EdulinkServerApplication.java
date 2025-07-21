e0f2db1c9fee3bbea17d8096d820f89e7b5f01b2
bascom241 <135514268+bascom241@users.noreply.github.com>
2025-07-21 16:34:40 +0100

    Project Setup

---
+++ EdulinkServer/src/main/java/com/Edulink/EdulinkServer/EdulinkServerApplication.java
@@ -0,0 +1,15 @@
+package com.Edulink.EdulinkServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdulinkServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdulinkServerApplication.class, args);
+		System.out.println("Welcome to Edu link Server Application");
+	}
+
+
+}
